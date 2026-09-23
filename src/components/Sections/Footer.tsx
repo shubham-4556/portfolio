e26d0b119cd83dc645b33d41924de42cd6b472f1
shubham-4556 <shubhamdeo728@gmail.com>
2026-09-23 11:25:30 +0530

    fix: remove reactresume attribution link from footer

--- src/components/Sections/Footer.tsx
+++ src/components/Sections/Footer.tsx
@@ -1,4 +1,4 @@
-import {BoltIcon, ChevronUpIcon} from '@heroicons/react/24/solid';
+import {ChevronUpIcon} from '@heroicons/react/24/solid';
 import {FC, memo} from 'react';
 
 import {homePageMeta, SectionId} from '../../data/data';
@@ -21,15 +21,6 @@ const Footer: FC = memo(() => {
         <div className="flex gap-x-4 text-neutral-500">
           <Socials />
         </div>
-        <a
-          className="-m-2 flex items-center gap-x-1 rounded-md p-2 ring-yellow focus:outline-none focus:ring-2"
-          href="https://reactresume.com">
-          <BoltIcon className="h-5 w-5 text-yellow" />
-          <span>
-            Provided by <span className="text-white">React</span>
-            <span className="italic text-yellow">Resume</span>
-          </span>
-        </a>
         <span className="text-sm text-neutral-700">
           © Copyright {currentYear} {name}
         </span>
